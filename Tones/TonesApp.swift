1d9696ce5824e00d9d9e3ce6d3bd462f94c88df2
Jamie White <jamie@jgwhite.co.uk>
2022-06-17 13:40:17 +0200

    Initial Commit

---
+++ Tones/TonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonesApp.swift
+//  Tones
+//
+//  Created by Jamie White on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
