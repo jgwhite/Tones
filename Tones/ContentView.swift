1eb69ec2f9e039504ddeee0875e8107db08994ef
Jamie White <jamie@jgwhite.co.uk>
2022-06-23 13:05:42 +0200

    Colors, colors, colors

--- Tones/ContentView.swift
+++ Tones/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @State var note = Note.C
+    
+    let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(note.letter)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text(note.letter)
+                    .font(.system(size: 100, design: .rounded))
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .drawingGroup()
+                Spacer()
+                    .frame(height: 100)
+                Text(note.solfege)
+                    .font(.system(size: 100, design: .serif))
+                    .italic()
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .drawingGroup()
+                Spacer()
+            }
+        }
+        .onReceive(timer) { _ in
+            var newNote = note
+            while newNote == note {
+                newNote = Note.allCases.randomElement()!
+            }
+            self.note = newNote
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
---
+++ Tones/Note.swift
@@ -0,0 +1,37 @@
+//
+//  Note.swift
+//  Tones
+//
+//  Created by Jamie White on 23/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum Note: CaseIterable {
+    case C, D, E, F, G, A, B
+    
+    var letter: String {
+        switch self {
+        case .C: return "C"
+        case .D: return "D"
+        case .E: return "E"
+        case .F: return "F"
+        case .G: return "G"
+        case .A: return "A"
+        case .B: return "B"
+        }
+    }
+    
+    var solfege: String {
+        switch self {
+        case .C: return "Do"
+        case .D: return "Re"
+        case .E: return "Me"
+        case .F: return "Fa"
+        case .G: return "Sol"
+        case .A: return "La"
+        case .B: return "Si"
+        }
+    }
+}
