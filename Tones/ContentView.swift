b62eb7f3e6eadd5057930be3df0d89be1d4985de
Jamie White <jamie@jgwhite.co.uk>
2022-06-27 16:57:39 +0200

    Add timer

--- Tones/ContentView.swift
+++ Tones/ContentView.swift
@@ -10,9 +10,17 @@ import Combine
 
 struct ContentView: View {
     
-    @State var note = Note.C
+    static let practiceSeconds = 30
     
-    let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    
+    @State var note = Note.C
+    @State var secondsRemaining = practiceSeconds
+    @State var isActive = true
+    @State var isTiming = false
+    
+    let seconds = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let notes = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -20,7 +28,27 @@ struct ContentView: View {
                 .ignoresSafeArea()
                 .zIndex(0)
             VStack {
+                if isTiming {
+                    Text(timerText())
+                        .foregroundColor(.white)
+                        .padding()
+                }
                 Spacer()
+                if isTiming {
+                    Button("Stop") { reset() }
+                        .buttonStyle(.bordered)
+                        .foregroundColor(.white)
+                } else {
+                    Button("Practise for 5 Minutes") {
+                        isTiming = true
+                    }
+                        .buttonStyle(.bordered)
+                        .foregroundColor(.white)
+                }
+
+            }
+            .padding()
+            VStack {
                 Text(note.letter)
                     .font(.system(size: 100, design: .rounded))
                     .foregroundColor(.white)
@@ -34,31 +62,79 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding(.horizontal)
                     .drawingGroup()
-                Spacer()
             }
             .id("note-\(note.letter)")
             .zIndex(1)
             .transition(.opacity.combined(with: .slide))
                         
         }
-        .onReceive(timer) { _ in
-            var newNote = note
-            while newNote == note {
-                newNote = Note.allCases.randomElement()!
+        .onReceive(seconds) { _ in
+            if !isActive {
+                return
+            }
+            if !isTiming {
+                return
             }
             
-            withAnimation {
-                self.note = newNote
+            if secondsRemaining > 0 {
+                secondsRemaining -= 1
+            } else {
+                reset()
+            }
+        }
+        .onReceive(notes) { _ in
+            if !isActive {
+                return
+            }
+            if !isTiming {
+                return
+            }
+
+            chooseNewNote()
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
     
+    func chooseNewNote() {
+        var newNote = note
+        while newNote == note {
+            newNote = Note.allCases.randomElement()!
+        }
+        withAnimation {
+            self.note = newNote
+        }
+    }
+    
+    func timerText() -> String {
+        let minutes = String(secondsRemaining / 60)
+        var seconds = String(secondsRemaining % 60)
+        
+        if seconds.count < 2 {
+            seconds = "0\(seconds)"
+        }
+        
+        return "\(minutes):\(seconds)"
+    }
+    
+    func reset() {
+        isTiming = false
+        secondsRemaining = ContentView.practiceSeconds
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
